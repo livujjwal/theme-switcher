34bb626dbe7b6543706e48fff90206758ef5a838
livujjwal <ujjwalsingh2112@gamil.com>
2023-10-25 13:32:55 +0530

    setup

---
+++ src/Components/Haven.js
@@ -0,0 +1,7 @@
+const Haven = () =>{
+    return (<div>
+        <h1>Welcome to heaven</h1>
+        <p>You gonna get a bones</p>
+    </div>)
+}
+export default Haven;
---
+++ src/Components/Hell.js
@@ -0,0 +1,7 @@
+const Hell = () =>{
+    return (<div>
+        <h1>Welcome to hell</h1>
+        <p>You gonna be gonner</p>
+    </div>)
+}
+export default Hell;
